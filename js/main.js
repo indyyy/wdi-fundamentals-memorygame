ced45a4d036eefefad8b897d22452334a7e51a83
Inder <inderyyz@gmail.com>
2018-06-24 15:27:33 +1000

    created game logic Unit 10

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"]; 
+// var cards = ["queen", "queen", "king", "king"]; 
 
-/*
+
 var cards = [
-{rank: 'queen',
+{
+rank: 'queen',
 suit: 'hearts',
 cardimage: 'images/queen-of-hearts.png'
 },
-{rank: 'queen'
-suit: 'diamonds'
+{
+rank: 'queen',
+suit: 'diamonds',
 cardimage: 'images/queen-of-diamonds.png'
 },
-{rank: 'king'
-suit: 'hearts'
+{
+rank: 'king',
+suit: 'hearts',
 cardimage: 'images/king-of-hearts.png'
 },
-{rank: 'king'
-suit: 'diamonds'
+{
+rank: 'king',
+suit: 'diamonds',
 cardimage: 'images/king-of-diamonds.png'
 }
 ];
-*/
+
 
 var cardsInPlay = [];
 
@@ -42,8 +46,10 @@ function checkForMatch() {
 
  function flipCard(cardId) { 
      
-    console.log("User flipped " + cards[cardId])
-    cardsInPlay.push(cards[cardId])
+    console.log("User flipped " + cards[cardId].rank)
+    cardsInPlay.push(cards[cardId].rank)
+    console.log(cards[cardId].cardimage)
+    console.log(cards[cardId].suit)
 	cardsInPlay.length;
 	if (cardsInPlay.length === 2)
 	{
